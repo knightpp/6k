adec468827d2d45d2866d51452541f0774256f3b
Danylo Kondratiev <knightpp@proton.me>
2024-05-26 17:23:01 +0300

    Update configs and enable rgb

--- config/rules.mk
+++ config/rules.mk
@@ -1,7 +1,4 @@
 # Build Options
 #   change yes to no to disable
 #
-MOUSEKEY_ENABLE = yes      	# Mouse keys
-EXTRAKEY_ENABLE = yes      	# Audio control and System control
-NKRO_ENABLE = yes           # Enable N-Key Rollover
 UNICODE_ENABLE = yes        # Unicode
